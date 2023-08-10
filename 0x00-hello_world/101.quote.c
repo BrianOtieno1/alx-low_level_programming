d7fb81cc6a764210e77bdb79fb305d545df4db62
BrianOtieno1 <brianodhiambo377@gmail.com>
2023-08-10 13:41:52 -0700

    101.quote.c 2

--- 0x00-hello_world/101.quote.c
+++ 0x00-hello_world/101.quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: 1 (error)
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(2, quote, 59);
-
+	
 	return (1);
 }
