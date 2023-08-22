53f7dfc2d5c1378249a75e81e866cd4ad057ea0b
BrianOtieno1 <brianodhiambo377@gmail.com>
2023-08-22 11:28:12 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - pointer to a character
+ *
+ * @dest: string 1 pointer to a char
+ * @src: string 2 pointer to a char
+ * Return: character (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\n'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\n';
+
+	return (dest);
+}
