e150066a4d191fe25eec5f07f3defcf2de939687
BrianOtieno1 <brianodhiambo377@gmail.com>
2023-08-31 00:59:29 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strbrk(char *s, char *accept);
-char *strstr(char *haystack, char *needle);
+char *_memset(char *s, char b, unsigned int n);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
